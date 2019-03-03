1305c0afc1f1e4ce21792f35f37c6df26fbbafc6
John Wu <john_t_wu@trend.com.tw>
2019-03-03 21:09:04 +0800

    first commit for captain marvel 20190306

---
+++ main.py
@@ -0,0 +1,27 @@
+#coding:utf-8
+import urllib2
+import time
+from bs4 import BeautifulSoup
+
+# main page http://www.vscinemas.com.tw/vsTicketing/ticketing/ticket.aspx
+# cinama 1:信義威秀 cinema=1|TP 2:京站威秀 cinema=12|QS
+quote_page = 'http://www.vscinemas.com.tw/vsTicketing/ticketing/ticket.aspx?cinema=12|QS&movie=HO00007641'
+
+page = urllib2.urlopen(quote_page)
+
+soup = BeautifulSoup(page, 'html.parser')
+
+# for Marvel studio, always find the first release day.
+movie_day_list = soup.find("div", {"class": "movieDay"})
+#movieDay = soup.find('movieDay')
+
+time_list = movie_day_list.find_all('li')
+
+print movie_day_list
+print len(time_list)
+
+# beside the Pre-sale ticket already release one week, we assume there might have other time will release later, count the total number bigger than.
+trigger_count = 6
+while (trigger_count < len(time_list)):
+	print("New Schedule!\a")
+	time.sleep(1)
